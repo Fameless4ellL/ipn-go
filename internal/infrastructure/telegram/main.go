1592c7963e758c9e819b6e7e89f02902e1d074fb
Zaur <fameless.4ell@gmail.com>
2026-03-26 15:43:16 +0300

    fix: proxy setup

--- internal/infrastructure/telegram/main.go
+++ internal/infrastructure/telegram/main.go
@@ -1,54 +1,33 @@
 package telegram
 
 import (
-	"context"
 	application "go-blocker/internal/application/payment"
 	"go-blocker/internal/infrastructure/telegram/handlers"
 	"go-blocker/internal/pkg/config"
 	"log"
-	"net"
 	"net/http"
+	"net/url"
 	"time"
 
-	"golang.org/x/net/proxy"
 	tele "gopkg.in/telebot.v4"
 )
 
 func Init(s *application.Service) {
-	proxyAddr := "72.56.93.128:1080"
-	auth := &proxy.Auth{
-		User:     "telegramik",
-		Password: "telegramik",
-	}
-
-	// 2. Create a SOCKS5 dialer
-	dialer, err := proxy.SOCKS5("tcp", proxyAddr, auth, proxy.Direct)
+	prx, err := url.Parse("socks5://telegramik:telegramik@72.56.93.128:1080")
 	if err != nil {
 		panic(err)
 	}
 
-	dialContext := func(ctx context.Context, network, address string) (net.Conn, error) {
-		return dialer.Dial(network, address)
-	}
-
-	transport := &http.Transport{
-		DialContext: dialContext,
-	}
-
-	proxyClient := &http.Client{
-		Transport: transport,
-		Timeout:   time.Second * 30,
-	}
-
 	pref := tele.Settings{
-		Client: proxyClient,
+		Client: &http.Client{
+			Transport: &http.Transport{Proxy: http.ProxyURL(prx)}},
 		Token:  config.BotToken,
 		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
 	}
 
 	b, err := tele.NewBot(pref)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
 		return
 	}
 
